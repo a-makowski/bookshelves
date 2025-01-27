367a562f38666b105f1f2c62b34beecea3341cb2
A. Makowski <a.makowski723@gmail.com>
2025-01-27 18:31:07 +0100

    Add new unit tests for RatingService

--- src/test/java/com/makowski/bookshelves/service/RatingServiceTest.java
+++ src/test/java/com/makowski/bookshelves/service/RatingServiceTest.java
@@ -3,7 +3,9 @@ package com.makowski.bookshelves.service;
 import com.makowski.bookshelves.entity.Book;
 import com.makowski.bookshelves.entity.Rating;
 import com.makowski.bookshelves.entity.User;
+import com.makowski.bookshelves.exceptions.AccessDeniedException;
 import com.makowski.bookshelves.exceptions.EntityNotFoundException;
+import com.makowski.bookshelves.exceptions.InvalidRequestException;
 import com.makowski.bookshelves.repository.RatingRepository;
 import com.makowski.bookshelves.testutils.TestDataFactory;
 import org.junit.jupiter.api.Test;
@@ -93,87 +95,243 @@ class RatingServiceTest {
 
     @Test
     void createRating_ReturnsRatingAndChangeBooksRate_WhenRatingWithScoreSuccessfullyCreated() {
+        User user = TestDataFactory.createTestUser();
+        Book book = TestDataFactory.createTestBook();
+        book.setScoresSum(10);
+        Rating rating = TestDataFactory.createTestRating();
+        rating.setScore(2);
 
+        when(userService.getLoggedUser()).thenReturn(user);
+        when(bookService.getBook(1L)).thenReturn(book);
+        when(ratingRepository.existsByUserAndBook(user, book)).thenReturn(false);
+        when(ratingRepository.save(rating)).thenReturn(rating);
+
+        Rating result = ratingService.createRating(rating, 1L);
+
+        assertEquals(user, result.getUser());
+        assertEquals(book, result.getBook());
+        assertEquals(6.0F, book.getRating());
     }
 
     @Test
     void createRating_ReturnsRatingAndDoesNotChangeBooksRate_WhenRatingWithoutScoreSuccessfullyCreated() {
+        User user = TestDataFactory.createTestUser();
+        Book book = TestDataFactory.createTestBook();
+        Rating rating = TestDataFactory.createTestRating();
+        rating.setReview("test");
 
+        when(userService.getLoggedUser()).thenReturn(user);
+        when(bookService.getBook(1L)).thenReturn(book);
+        when(ratingRepository.existsByUserAndBook(user, book)).thenReturn(false);
+        when(ratingRepository.save(rating)).thenReturn(rating);
+
+        Rating result = ratingService.createRating(rating, 1L);
+
+        assertEquals(user, result.getUser());
+        assertEquals(book, result.getBook());
+        assertEquals(10.0F, book.getRating());
     }
 
     @Test
     void createRating_ThrowsException_WhenRatingFromThisUserForThisBookAlreadyExists() {
+        User user = TestDataFactory.createTestUser();
+        Book book = TestDataFactory.createTestBook();
+        Rating rating = TestDataFactory.createTestRating();
 
+        when(userService.getLoggedUser()).thenReturn(user);
+        when(bookService.getBook(1L)).thenReturn(book);
+        when(ratingRepository.existsByUserAndBook(user, book)).thenReturn(true);
+
+        assertThrows(InvalidRequestException.class, () -> ratingService.createRating(rating, 1L));
     }
 
     @Test
     void createRating_ThrowsException_WhenRatingIsIncorrect() {
+        User user = TestDataFactory.createTestUser();
+        Book book = TestDataFactory.createTestBook();
+        Rating rating = TestDataFactory.createTestRating();
 
+        when(userService.getLoggedUser()).thenReturn(user);
+        when(bookService.getBook(1L)).thenReturn(book);
+        when(ratingRepository.existsByUserAndBook(user, book)).thenReturn(false);
+
+        assertThrows(InvalidRequestException.class, () -> ratingService.createRating(rating, 1L));
     }
 
     @Test
     void updateRating_ReturnsEditedRatingAndChangeBooksRate_WhenScoreWasUpdated() {
+        Book book = TestDataFactory.createTestBook();
+        book.setScoresSum(10);
+        User user = TestDataFactory.createTestUser();
+        Rating rating = TestDataFactory.createTestRating();
+        rating.setUser(user);
+        rating.setBook(book);
+        Rating newRating = new Rating();
+        newRating.setScore(2);
 
+        when(ratingRepository.findById(1L)).thenReturn(Optional.of(rating));
+        when(userService.getLoggedUser()).thenReturn(user);
+        when(ratingRepository.save(rating)).thenReturn(rating);
+
+        Rating result = ratingService.updateRating(1L, newRating);
+
+        assertEquals(1L, result.getId());
+        assertEquals(2, result.getScore());
+        assertEquals(6.0F, book.getRating());
     }
 
     @Test
     void updateRating_ReturnsEditedRatingAndDoesNotChangeBooksRate_WhenScoreWasNotUpdated() {
+        User user = TestDataFactory.createTestUser();
+        Rating rating = TestDataFactory.createTestRating();
+        rating.setUser(user);
+        Rating newRating = new Rating();
+        newRating.setReview("test");
 
-    }
+        when(ratingRepository.findById(1L)).thenReturn(Optional.of(rating));
+        when(userService.getLoggedUser()).thenReturn(user);
+        when(ratingRepository.save(rating)).thenReturn(rating);
 
-    @Test
-    void updateRating_ThrowsException_WhenRatingDoesNotExist() {
+        Rating result = ratingService.updateRating(1L, newRating);
 
+        assertEquals(1L, result.getId());
+        assertEquals("test", result.getReview());
     }
 
     @Test
     void updateRating_ThrowsException_WhenUserIsNotOwner() {
+        User loggedUser = TestDataFactory.createTestUser();
+        User ratingOwner = TestDataFactory.createAnotherTestUser();
+        Rating rating = TestDataFactory.createTestRating();
+        rating.setUser(ratingOwner);
+        Rating newRating = new Rating();
 
+        when(ratingRepository.findById(1L)).thenReturn(Optional.of(rating));
+        when(userService.getLoggedUser()).thenReturn(loggedUser);
+
+        assertThrows(AccessDeniedException.class, () -> ratingService.updateRating(1L, newRating));
     }
 
     @Test
     void updateRating_ThrowsException_WhenRatingIsNotValid() {
+        User user = TestDataFactory.createTestUser();
+        Rating rating = TestDataFactory.createTestRating();
+        rating.setUser(user);
+        Rating newRating = new Rating();
 
+        when(ratingRepository.findById(1L)).thenReturn(Optional.of(rating));
+        when(userService.getLoggedUser()).thenReturn(user);
+
+        assertThrows(InvalidRequestException.class, () -> ratingService.updateRating(1L, newRating));
     }
 
     @Test
     void deleteRating_DeletesRatingAndChangesBooksRate_WhenRatingContainsScore() {
+        Book book = TestDataFactory.createTestBook();
+        User user = TestDataFactory.createTestUser();
+        Rating rating = TestDataFactory.createTestRating();
+        rating.setUser(user);
+        rating.setBook(book);
+        rating.setScore(10);
+
+        when(userService.getLoggedUser()).thenReturn(user);
+        when(ratingRepository.existsById(1L)).thenReturn(true);
+        when(ratingRepository.findById(1L)).thenReturn(Optional.of(rating));
+
+        ratingService.deleteRating(1L);
+
+        verify(ratingRepository).deleteById(1L);
+        assertEquals(0.0F, book.getRating());
 
     }
 
     @Test
     void deleteRating_DeletesRatingAndDoesNotChangeBookRate_WhenRatingDoesNotContainScore() {
+        Book book = TestDataFactory.createTestBook();
+        User user = TestDataFactory.createTestUser();
+        Rating rating = TestDataFactory.createTestRating();
+        rating.setUser(user);
+        rating.setBook(book);
 
+        when(userService.getLoggedUser()).thenReturn(user);
+        when(ratingRepository.existsById(1L)).thenReturn(true);
+        when(ratingRepository.findById(1L)).thenReturn(Optional.of(rating));
+
+        ratingService.deleteRating(1L);
+
+        verify(ratingRepository).deleteById(1L);
+        assertEquals(10.0F, book.getRating());
     }
 
     @Test
     void deleteRating_ThrowsException_WhenRatingDoesNotExist() {
+        when(ratingRepository.existsById(1L)).thenReturn(false);
 
+        assertThrows(EntityNotFoundException.class, () -> ratingService.deleteRating(1L));
     }
 
     @Test
     void deleteRating_ThrowsException_WhenUserDoesNotOwnRating() {
+        User loggedUser = TestDataFactory.createTestUser();
+        User ratingOwner = TestDataFactory.createAnotherTestUser();
+        Rating rating = TestDataFactory.createTestRating();
+        rating.setUser(ratingOwner);
 
+        when(userService.getLoggedUser()).thenReturn(loggedUser);
+        when(ratingRepository.existsById(1L)).thenReturn(true);
+        when(ratingRepository.findById(1L)).thenReturn(Optional.of(rating));
+
+        assertThrows(AccessDeniedException.class, () -> ratingService.deleteRating(1L));
     }
 
     @Test
     void changeRating_changesBooksRating_WhenScoreWasAdded() {
+        Book book = TestDataFactory.createTestBook();
+        book.setScoresSum(10);
 
+        ratingService.changeRating(book, 0, 2);
+
+        assertEquals(2, book.getScoresNumber());
+        assertEquals(12, book.getScoresSum());
+        assertEquals(6.0F, book.getRating());
     }
 
     @Test
     void changeRating_changesBooksRating_WhenScoreWasDeletedAndThereIsNoMoreScores() {
+        Book book = TestDataFactory.createTestBook();
+        book.setScoresSum(10);
 
+        ratingService.changeRating(book, 10, 0);
+
+        assertEquals(0, book.getScoresNumber());
+        assertEquals(0, book.getScoresSum());
+        assertEquals(0, book.getRating());
     }
 
     @Test
     void changeRating_changesBooksRating_WhenScoreWasDeletedAndThereAreAnotherScores() {
+        Book book = TestDataFactory.createTestBook();
+        book.setScoresSum(12);
+        book.setScoresNumber(2);
 
+        ratingService.changeRating(book, 10, 0);
+
+        assertEquals(1, book.getScoresNumber());
+        assertEquals(2, book.getScoresSum());
+        assertEquals(2.0F, book.getRating());
     }
 
     @Test
     void changeRating_changesBooksRating_WhenScoreWasEdited() {
+        Book book = TestDataFactory.createTestBook();
+        book.setScoresSum(12);
+        book.setScoresNumber(2);
 
+        ratingService.changeRating(book, 10, 6);
+
+        assertEquals(2, book.getScoresNumber());
+        assertEquals(8, book.getScoresSum());
+        assertEquals(4.0F, book.getRating());
     }
 
     @Test
