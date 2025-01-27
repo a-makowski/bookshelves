99bca2c824e6bbc2046ccb328ffb956b163d6a3b
A. Makowski <a.makowski723@gmail.com>
2025-01-27 20:27:41 +0100

    Add AllArgsConstructor to PasswordDto class

--- src/main/java/com/makowski/bookshelves/dto/PasswordDto.java
+++ src/main/java/com/makowski/bookshelves/dto/PasswordDto.java
@@ -1,10 +1,12 @@
 package com.makowski.bookshelves.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class PasswordDto {
     
     private String oldPassword;
